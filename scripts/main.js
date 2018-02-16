1e994d8ec449b6ff48bb7aa0a35f3ebf619a7b43
RustFelipe <36306945+RustFelipe@users.noreply.github.com>
2018-02-15 19:51:01 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/jiujitsu.jpg') {
+      myImage.setAttribute ('src','images/jiujitsu3.jpg');
+    } else {
+      myImage.setAttribute ('src','images/jiujitsu.jpg');
+    }
+}
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Jiu Jitsu is awesome, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Jiu Jitsu is awesome, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
